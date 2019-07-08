a5ce754173698f3f01f89f63207d6e2b725adbe0
Thomas O Fredericks <tof@t-o-f.info>
2019-07-08 10:19:51 -0400

    Update tinySLIP.h

--- src/tinySLIP.h
+++ src/tinySLIP.h
@@ -5,6 +5,8 @@
 #include "WProgram.h"
 #endif
 
+#include "UDP.h"
+
 // Internal. SLIP reserved codes.
 // 192, 219, 220, 221
 #define SLIP_END     0xC0
@@ -12,9 +14,11 @@
 #define SLIP_ESC_END 0xDC
 #define SLIP_ESC_ESC 0xDD
 
+#ifndef TINYSLIP_BUFFER_SIZE 
+#define TINYSLIP_BUFFER_SIZE 512
+#endif
 
-
-class TinySlip {
+class TinySlip : UDP {
 
  private:
 	
@@ -22,16 +26,21 @@ class TinySlip {
 	size_t parseIndex=0;
 	bool error = false;
 	bool escaping =false;
-
+    Stream* serial;
+    unsigned char buffer[TINYSLIP_BUFFER_SIZE];
 	
 	public:
+      TinySlip( Stream* serial ) {
+      	this->serial = serial;
+      }
+		
 
 		size_t available() {
 			return length;
 		}
 
 		// THIS COULD BE OPTIMISED
-		bool parseStream( Stream* serial, unsigned char *buffer, size_t maxLength) {
+		bool parseStream( ) {
 
 
 		    while ( serial->available()>0 ) {
@@ -77,64 +86,7 @@ class TinySlip {
 
     return false;
 
-				/*
-			while ( stream->available() ) {
-				int streamByte = stream->read();
 				
-				// WAITING
-				if ( parseStatus == TINY_PARSE_WAITING ) {
-					// ONLY START PACKING ONCE WE GET A START/END MARKER
-					 if ( streamByte == SLIP_END ) {
-							parseStatus = TINY_PARSE_BUILDING;
-							length = parseIndex = 0;
-					 }
-				// BUILDING OR ESCAPING
-				} else {
-					// END OF MESSAGE, RETURN NUMBER OF BYTES
-					if ( streamByte == SLIP_END ) {
-						parseStatus = TINY_PARSE_WAITING;
-
-						length = parseIndex;
-						
-
-						return length>0;
-
-					// MESSAGE DATA
-					 } else {
-						// ESCAPING
-						 if ( parseStatus == TINY_PARSE_ESCAPING  ) {
-								switch (streamByte) {
-								case SLIP_ESC_END:
-									tinystore(SLIP_END);
-									break;
-								case SLIP_ESC_ESC:
-									tinystore(SLIP_ESC);
-									break;
-						 }
-							parseStatus = TINY_PARSE_BUILDING;
-						 } else {
-							// NON ESCAPING
-								if ( streamByte == SLIP_ESC ) {
-									parseStatus = TINY_PARSE_ESCAPING;
-								} else {
-									tinystore(streamByte);
-								}
-						}
-				}
-
-				// ERROR
-				if ( parseIndex >= maxLength ) {
-						parseStatus = TINY_PARSE_WAITING;
-				}
-
-				
-
-			}
-
-			
-		}
-		return 0;
-		*/
 	}
 
 	void streamPacket (Stream* stream, unsigned char *buffer, size_t length) {
